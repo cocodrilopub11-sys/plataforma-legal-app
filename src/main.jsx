814f10b4e0b35ed5abe3071e87cec2ba0ffa4d38
cocodrilopub11-sys <cocodrilopub11@gmail.com>
2025-11-26 14:47:07 -0300

    Arreglando estilos visuales final

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // <--- ¡ESTA LÍNEA ES LA CLAVE!
+import './index.css' // Aquí está nuestro diseño dorado
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
