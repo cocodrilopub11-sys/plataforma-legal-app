024ce0efa8a3e5ad1595d4f0b270c59c4bbf4c7d
cocodrilopub11-sys <cocodrilopub11@gmail.com>
2025-11-26 22:43:24 -0300

    Arreglando referencias en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LegalApp from './LegalApp.jsx' // <--- ¡AQUÍ ESTÁ LA CLAVE!
-import './index.css'
+import LegalApp from './LegalApp.jsx'
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
